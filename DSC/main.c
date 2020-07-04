ffc3e285074601e904ce01e6377c3cc33e6826e9
Ahmed Shalaby <kingshalaby@yahoo.com>
2020-07-04 14:11:57 +0200

    Initial Commit

---
+++ DSC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DSC
+//
+//  Created by ahmed shalaby on 7/4/20.
+//  Copyright © 2020 ahmed shalaby. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
